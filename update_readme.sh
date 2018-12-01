a3f898fbe4d45427a5256f0f5c8d29d742974de5
Lichtkind <lichtkind@cpan.org>
2018-12-01 23:20:01 +0100

    remove appvayor

--- update_readme.sh
+++ update_readme.sh
@@ -4,7 +4,7 @@ echo "Generating README.md"
 
 echo "[![Build Status](https://travis-ci.org/lichtkind/Perl6-Math-Matrix-Bundle.svg?branch=master)](https://travis-ci.org/lichtkind/Perl6-Math-Matrix-Bundle)" >README.md
 
-echo "[![Build status](https://ci.appveyor.com/api/projects/status/github/lichtkind/Perl6-Math-Matrix-Bundle?svg=true)](https://ci.appveyor.com/project/lichtkind/Perl6-Math-Matrix-Bundle/branch/master)\n" >>README.md
+#echo "[![Build status](https://ci.appveyor.com/api/projects/status/github/lichtkind/Perl6-Math-Matrix-Bundle?svg=true)](https://ci.appveyor.com/project/lichtkind/Perl6-Math-Matrix-Bundle/branch/master)\n" >>README.md
 
 perl6 --doc=Markdown lib/Math/Matrix.pod >>README.md
 
